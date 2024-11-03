9e643ac75e07da06ca59d153217a4a3ceed4e693
Marcel-Mattos <marcel_mattos@hotmail.com>
2024-11-03 16:42:42 -0300

    Adicionando a tela "sobre o desenvolvedor"

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,9 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new Sobre());
+    }
 }
